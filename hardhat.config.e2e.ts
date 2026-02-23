4972e7f74ef9ba7477550364da8bdd5ebd40feb9
Loïc Vincent-Genod <37535451+VGLoic@users.noreply.github.com>
2026-02-23 23:41:23 +0100

    Integrate Ethoko, add e2e tests (#1)
    * update lock
    
    * set up hardhat-ethoko
    
    * update hardhat ethoko version
    
    * set up end to end test
    
    * set up e2e workflow with dispatch
    
    * remove uneeded deps

---
+++ hardhat.config.e2e.ts
@@ -0,0 +1,46 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { configVariable, defineConfig } from "hardhat/config";
+import HardhatEthoko from "hardhat-ethoko";
+
+export default defineConfig({
+  plugins: [hardhatToolboxViemPlugin, HardhatEthoko],
+  ethoko: {
+    project: "uniswap-v4-core-ethoko-test",
+    pulledArtifactsPath: "./ethoko-e2e/.ethoko",
+    typingsPath: "./.ethoko-typings",
+    compilationOutputPath: "./out",
+    storageConfiguration: {
+      type: "local",
+      path: "./ethoko-e2e/.storage",
+    },
+  },
+  solidity: {
+    // Profiles are mandatory but are not used here since we build with `Forge
+    profiles: {
+      default: {
+        version: "0.8.26",
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: {
+        mnemonic: configVariable("SEPOLIA_MNEMONIC"),
+      },
+    },
+  },
+  paths: {
+    sources: "./src", // Use ./src rather than ./contracts as Hardhat expects
+  },
+});
